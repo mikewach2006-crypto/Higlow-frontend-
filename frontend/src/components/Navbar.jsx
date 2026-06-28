23d2690b96041c23854ac6e60e137cddd97458d4
Wachira Michael Munene <mikewach2006@gmail.com>
2026-06-28 17:02:55 +0300

    Add Navbar component for site header

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+function Navbar() {
+  return (
+    <header className="navbar">
+
+      <button className="menu">
+
+☰
+
+      </button>
+
+      <h2>Higlow Business</h2>
+
+      <div className="profile">
+
+👤
+
+      </div>
+
+    </header>
+  );
+}
+
+export default Navbar;
